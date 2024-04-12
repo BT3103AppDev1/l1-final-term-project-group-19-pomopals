ef3b1cf5b64b1a83be3c9421c97ae37adee838c2
edwinhrj <122248645+edwinhrj@users.noreply.github.com>
2024-04-13 00:14:17 +0800

    host and member page
    host and member page

--- pomopals/src/router/index.js
+++ pomopals/src/router/index.js
@@ -7,8 +7,10 @@ import FriendsPage from "@/views/FriendsPage.vue";
 import LeaderboardPage from "@/views/LeaderboardPage.vue";
 import FriendsLeaderboardPage from "@/views/FriendsLeaderboardPage.vue";
 import URLBlockerPage from "@/views/URLBlockerPage.vue";
-import AchievementPage from "@/views/Achievements.vue"
+import AchievementPage from "@/views/Achievements.vue";
 import AllAchievementPage from "@/views/AllAchievements.vue";
+import HostHomePage from "@/views/HostHomePage.vue";
+import MemberHomePage from "@/views/MemberHomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +36,16 @@ const router = createRouter({
       component: HomePage,
     },
     {
+      path: "/host",
+      name: "HostHomePage",
+      component: HostHomePage,
+    },
+    {
+      path: "/member",
+      name: "MemberHomePage",
+      component: MemberHomePage,
+    },
+    {
       path: "/friends",
       name: "FriendsPage",
       component: FriendsPage,
@@ -54,13 +66,13 @@ const router = createRouter({
       component: URLBlockerPage,
     },
     {
-      path: "/achievements", 
-      name: "AchievementPage", 
+      path: "/achievements",
+      name: "AchievementPage",
       component: AchievementPage,
-    }, 
+    },
     {
-      path: "/allachievements", 
-      name: "AllAchievementPage", 
+      path: "/allachievements",
+      name: "AllAchievementPage",
       component: AllAchievementPage,
     },
   ],
---
+++ pomopals/src/views/HostHomePage.vue
@@ -0,0 +1,46 @@
+<script>
+import Timer from "/src/components/Timer.vue";
+import XpBar from "/src/components/XpBar.vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "HostHomePage",
+  components: { XpBar, Timer, NavBar },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="home">
+    <div><NavBar /></div>
+    <div>
+      <h1 id="groupStudy">
+        <bold> YOU ARE THE HOST OF THE STUDY SESSION </bold>
+      </h1>
+    </div>
+    <div><XpBar /></div>
+    <div><Timer ref="timerRef" /></div>
+  </div>
+</template>
+
+<style scoped>
+#friendsButton,
+#leaderboardButton {
+  position: absolute;
+  right: 10px;
+}
+
+#friendsButton {
+  top: 19%;
+  transform: translateY(-100%);
+}
+
+#leaderboardButton {
+  top: 19%;
+}
+
+#groupStudy {
+  color: black;
+  font-size: 100px;
+}
+</style>
